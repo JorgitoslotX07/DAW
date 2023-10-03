9fd3c6344abea9e72dbc0e171ba6ec8d03972e28
JorgitoslotX07 <97570939+JorgitoslotX07@users.noreply.github.com>
2023-10-03 20:20:29 +0200

    Add files via upload

---
+++ M03/UF4/pt1ej4.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class pt1ej4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese la cantidad de dinero -->  ");
+        float x = sc.nextFloat();
+        System.out.print("Indique una tasa de interes -->  ");
+        float y = sc.nextFloat();
+        System.out.print("Indique la cantidad de años -->  ");
+        float z = sc.nextFloat();
+        float xyz = x * (1 + y/100);
+        System.out.println(Math.pow(xyz, z));
+        sc.close();
+    }
+}
