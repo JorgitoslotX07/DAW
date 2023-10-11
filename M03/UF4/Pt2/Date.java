2fecbc7d333db4032f50078d0ccaceacec4dd55c
JorgitoslotX07 <97570939+JorgitoslotX07@users.noreply.github.com>
2023-10-11 19:11:16 +0200

    Add files via upload

---
+++ M03/UF4/Pt2/Date.java
@@ -0,0 +1,32 @@
+package dawmainm03.M03.UF4.Pt2;
+
+public class Date {
+    private Integer dia;
+    private Integer mes;
+    private Integer any;
+
+    public Integer getDia() {
+        return dia;
+    }
+
+    public Integer getMes() {
+        return mes;
+    }
+
+    public Integer getAny() {
+        return any;
+    }
+
+    public void setDia(Integer d) {
+        this.dia = d;
+    }
+
+    public void setMes(Integer m) {
+        this.mes = m;
+    }
+
+    public void setAny(Integer a) {
+        this.any = a;
+    }
+
+}
---
+++ M03/UF4/Pt2/Media.java
@@ -0,0 +1,59 @@
+package dawmainm03.M03.UF4.Pt2;
+
+public class Media {
+    private String nom;
+    private String autor;
+    private Integer duradaSegons;
+
+    public Media(String n, String a, Integer d) {
+        this.setNom(n);
+        this.setAutor(a);
+        this.setDurada(d);
+    }
+
+    public static final void main(String[] args) {
+
+        media m = new media("Pendejada 4000", "Toni Jorda Leon", 120);
+
+        System.out.println("\n" + m.getNom() + " " + m.getAutor() + " " + m.getDurada());
+
+        System.out.println("\n-----------------------------------\n");
+        
+        System.out.println(m.toString());
+
+    }
+
+    public String getNom() {
+        return this.nom;
+    }
+
+    public String getAutor() {
+        return this.autor;
+    }
+
+    public Integer getDurada() {
+        return this.duradaSegons;
+    }
+
+
+    public void setNom(String n) {
+        this.nom = n;
+    }
+
+    public void setAutor(String a) {
+        this.autor = a;
+    }
+
+    public void setDurada(Integer d) {
+        this.duradaSegons = d;
+    }
+
+    @Override
+    public String toString() {
+        return "Nom: " + this.getNom() + 
+            "\nAutor: " + this.getAutor() + 
+            "\nDurada: " + this.getDurada()
+        ;
+    }
+
+}
