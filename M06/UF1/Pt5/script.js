66f2627e08273612209231c090d508cc50142832
JorgitoslotX07 <97570939+JorgitoslotX07@users.noreply.github.com>
2023-11-02 21:15:39 +0100

    Add files via upload

--- M06/UF1/Pt5/script.js
+++ M06/UF1/Pt5/script.js
@@ -16,9 +16,22 @@ function getPortatiles() {
         if (key.startsWith("portatil")) {
             let portatilString = localStorage.getItem(key);
             let portatil = JSON.parse(portatilString);
-            portatiles.push(portatil);
+            portatiles.push(
+                {
+                    key: key,
+                    portatil: portatil
+                }
+            );
         }
     }
+
+    // Ordenar portatiles
+    portatiles.sort((a, b) => {
+        let keyA = parseInt(a.key.replace('portatil', ''), 10);
+        let keyB = parseInt(b.key.replace('portatil', ''), 10);
+        return keyA - keyB;
+    });
+
 
     // let portatilesString = localStorage.getItem('portatiles');
     // let portatiles = JSON.parse(portatilesString);
@@ -27,12 +40,12 @@ function getPortatiles() {
     return portatiles;
 }
 
-function setPortatiles(portatil) {
+function setPortatiles(por) {
 
     let key = localStorage.getItem('key') || 1;
     let portatilKey = "portatil" + key;
 
-    localStorage.setItem(portatilKey, JSON.stringify(portatil));
+    localStorage.setItem(portatilKey, JSON.stringify(por));
 
     key++;
     localStorage.setItem('key', key);
@@ -54,6 +67,7 @@ function setPortatiles(portatil) {
 function guardarPortatil() {
 
     let marca = document.getElementById('marca').value;
+    console.log(marca);
     let modelo = document.getElementById('modelo').value;
     let modeloCPU = document.getElementById('modeloCPU').value;
     let ram = document.getElementById('ram').value;
@@ -103,11 +117,12 @@ function valueForm(marca = null, modelo = null, modeloCPU = null, ram = null,
 function ultimoPortatil() {
     let portatiles = getPortatiles();
     
-    if (portatiles == null) {
+    if (portatiles == null || portatiles.length == 0) {
         return null;
     }
     else {
-        let ultimoPortatil = portatiles[portatiles.length - 1];
+        let ultimoPortatil = portatiles
+            [portatiles.length - 1]['portatil'];
         return ultimoPortatil;  
     }
 }
@@ -129,4 +144,83 @@ function reiniciarPortatiles() {
     localStorage.clear();
 
     onLoad();
+}
+
+function get2PortatilesAleatorios() {
+    let portatiles = getPortatiles();
+    let numeros = get2NumerosAleatorios(portatiles.length - 1);
+    
+    return {
+        port1: portatiles[numeros[0]],
+        port2: portatiles[numeros[1]]
+    }
+}
+
+function get2NumerosAleatorios(max) {
+    let num1 = getNumeroAleatorio(max);
+    let num2 = num1;
+
+    while (num2 == num1) {
+        num2 = getNumeroAleatorio(max);
+    }
+
+    return [num1, num2];
+}
+
+function getNumeroAleatorio(max){
+    let num = Math.floor(Math.random() * max) + 1;
+    return num;
+}
+
+function tablaPortatiles() {
+    
+    let ports = get2PortatilesAleatorios();
+
+    let port1 = ports.port1.portatil;
+    let port2 = ports.port2.portatil;
+    let keys = Object.keys(ports.port1.portatil)
+
+
+    // let ventanaEmergente = window.open('', 
+    //     '_blank', 'width=400,height=400');
+    let tabla = document.createElement('table');
+    let fila1 = tabla.insertRow();
+    let fila2 = tabla.insertRow();
+    let fila3 = tabla.insertRow();
+
+    // Poner keys en tabla
+    // keys.forEach(e => {
+    //     fila1.insertCell().textContent = e;
+    // });
+    introducirFila(keys, fila1);
+
+    console.log(port1)
+    // Poner keys en tabla
+    // port1.forEach(e => {
+    //     fila2.insertCell().textContent = e;
+    // });
+    introducirPortatilFila(port1, fila2);
+
+    // Poner keys en tabla
+    // port2.forEach(e => {
+    //     fila3.insertCell().textContent = e;
+    // });
+    introducirPortatilFila(port1, fila3);
+    
+    document.body.appendChild(tabla);
+}
+
+function introducirPortatilFila(port, fila) {
+    list = [];
+    for (let key in port) {
+        list.push(port[key]);
+    }
+
+    introducirFila(list, fila);
+}
+
+function introducirFila(list, fila) {
+    list.forEach(e => {
+        fila.insertCell().textContent = e;
+    });
 }
