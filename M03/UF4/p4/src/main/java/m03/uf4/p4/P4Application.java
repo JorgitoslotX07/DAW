3c581a6000bad7e9005508d6e7b285aaa41a014c
JorgitoslotX07 <97570939+JorgitoslotX07@users.noreply.github.com>
2023-11-22 19:14:44 +0100

    Add files via upload

---
+++ M03/UF4/p4/src/main/java/m03/uf4/p4/P4Application.java
@@ -0,0 +1,14 @@
+package m03.uf4.p4;
+
+// import org.springframework.boot.SpringApplication;
+// import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// @SpringBootApplication
+public class P4Application {
+
+	public static void main(String[] args) {
+		// SpringApplication.run(P4Application.class, args);
+		System.out.println("Main app");
+	}
+
+}
