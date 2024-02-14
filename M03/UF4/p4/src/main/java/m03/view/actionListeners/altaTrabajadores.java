c3e380666ee251062c9c58c84c1f01a15d550073
JorgitoslotX07 <97570939+JorgitoslotX07@users.noreply.github.com>
2024-02-14 20:11:40 +0100

    Add files via upload

--- M03/UF4/p4/src/main/java/m03/view/actionListeners/altaTrabajadores.java
+++ M03/UF4/p4/src/main/java/m03/view/actionListeners/altaTrabajadores.java
@@ -2,7 +2,6 @@ package m03.view.actionListeners;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-
 import org.elasticsearch.client.RestClient;
 import java.awt.*;
 import java.awt.event.*;
@@ -19,18 +18,18 @@ public class altaTrabajadores extends JFrame {
     private JTextField tDataNaixement;
     private JTextField tDataIniciContracte;
     private JTextField tSalariBase;
-    private JLabel l3;
+    private JCheckBox cbEncarregat;
 
     public altaTrabajadores(Encarregat encarregat) {
         super("Agregar Trabajador");
 
-        setSize(600, 600);
+        setSize(600, 230);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         Container cp = getContentPane();
 
         cp.setLayout(new BorderLayout());
         JPanel p1 = new JPanel();
-        p1.setLayout(new GridLayout(6, 2)); 
+        p1.setLayout(new GridLayout(7, 2)); // Incrementat per incloure el checkbox
 
         JLabel lNom = new JLabel("Nombre: ");
         tNom = new JTextField();
@@ -62,23 +61,44 @@ public class altaTrabajadores extends JFrame {
         p1.add(lSalariBase);
         p1.add(tSalariBase);
 
+        JLabel lEncarregat = new JLabel("Encarregat: ");
+        cbEncarregat = new JCheckBox();
+        p1.add(lEncarregat);
+        p1.add(cbEncarregat);
+
         JButton b1 = new JButton("Aceptar");
         b1.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                String nom = tNom.getText();
-                String cognoms = tCognoms.getText();
-                String dni = tDni.getText();
-                LocalDate dataNaixement = LocalDate.parse(tDataNaixement.getText());
-                LocalDate dataIniciContracte = LocalDate.parse(tDataIniciContracte.getText());
-                double salariBase = Double.parseDouble(tSalariBase.getText());
-
-                System.out.println("Nombre: " + nom);
-                System.out.println("Apellidos: " + cognoms);
-                System.out.println("DNI: " + dni);
-                System.out.println("Fecha de Nacimiento: " + dataNaixement);
-                System.out.println("Fecha de Inicio de Contrato: " + dataIniciContracte);
-                System.out.println("Salario Base: " + salariBase);
+                if (validarDades()) {
+                    if (cbEncarregat.isSelected()) {
+                        Treballador treNew = new Treballador(
+                            tNom.getText(),
+                            tCognoms.getText(),
+                            tDni.getText(),
+                            LocalDate.parse(tDataNaixement.getText()),
+                            LocalDate.parse(tDataIniciContracte.getText()),
+                            Double.parseDouble(tSalariBase.getText())
+                        );
+                        encarregat.afegirTreballador(treNew);
+                    } else {
+                        Encarregat encNew = new Encarregat(
+                            tNom.getText(),
+                            tCognoms.getText(),
+                            tDni.getText(),
+                            LocalDate.parse(tDataNaixement.getText()),
+                            LocalDate.parse(tDataIniciContracte.getText()),
+                            Double.parseDouble(tSalariBase.getText()),
+                            500
+                        );
+                        encarregat.afegirTreballador(encNew);
+                    }
+                    
+                    dispose();
+                    Ventana ventana = new Ventana(encarregat);
+                    ventana.setVisible(true);
+                    
+                }
             }
         });
 
@@ -86,5 +106,23 @@ public class altaTrabajadores extends JFrame {
         cp.add(b1, BorderLayout.CENTER);
 
         setVisible(true);
+    }
+
+    private boolean validarDades() {
+        if (tNom.getText().isEmpty() || tCognoms.getText().isEmpty() || tDni.getText().isEmpty() ||
+            tDataNaixement.getText().isEmpty() || tDataIniciContracte.getText().isEmpty() || 
+            tSalariBase.getText().isEmpty()) {
+            JOptionPane.showMessageDialog(this, "Falten dades");
+            return false;
+        }
+        try {
+            LocalDate.parse(tDataNaixement.getText());
+            LocalDate.parse(tDataIniciContracte.getText());
+            Double.parseDouble(tSalariBase.getText());
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(this, "Les dates o el salari no son valid");
+            return false;
+        }
+        return true;
     }
 }
